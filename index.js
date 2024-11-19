6a5934e6a31316a042a42cf9fa81b5c27b792577
Jose Soto <josesotogamez@gmail.com>
2024-11-19 16:27:27 -0500

    agregando link Usuarios

--- index.js
+++ index.js
@@ -5,4 +5,7 @@ const app = express();
 app.get('/', (req, res)=>{
     res.status(200).send('<h2>Estas en la pagina Principal</h2>')
 })
+app.get('/usuario', (req, res)=>{
+    res.status(200).send('<h2>Dentro de la seccion Usuarios</h2>')
+})
 app.listen(3000, ()=>console.log("🖥servidor levantado..."))
